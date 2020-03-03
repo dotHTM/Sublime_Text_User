5dcbcff8192a6deabef56f8b0f307528a607e924
Mike Cramer <mcramer@austin.utexas.edu>
2020-03-03 10:43:53 -0600

    snapshot

--- additions_mlc/languages/perl/build/perltidy.sh
+++ additions_mlc/languages/perl/build/perltidy.sh
@@ -6,10 +6,12 @@
 #   Started on DATE
 
 
+perltidy_bin='/usr/local/bin/perltidy'
+
 main(){
   for filepath in $*; do
     if [[ -n $filepath && -e $filepath ]]; then
-      result=`perltidy -se -b $filepath 2>&1`
+      result=`$perltidy_bin -se -b $filepath 2>&1`
       if [[ -n $result ]]; then
         echo "$filepath"
         echo "$result"
